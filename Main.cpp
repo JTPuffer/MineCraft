1f7f9121ff69e592b75050b341acb864c2d20519
JTPuffer <m.oliver4@lancaster@ac.uk>
2023-12-19 11:09:06 +0000

    Made it so it acctually works i dont know what was wrong with teh last commit.

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,28 @@
 #include <glad/glad.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include "Shader.h"
 
+#define Width 1000
+#define Height 1000
 
+float vertices[] = {
+    // positions         // colors
+     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+};
+
+unsigned int indices[] = { // note that we start from 0!
+0, 1, 3, // first triangle
+1, 2, 3 // second triangle
+};
 
 int main(void)
 {
-	glfwInit();
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);//option then the value 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);// only get accses to a subset of functions
 
 
@@ -39,17 +53,23 @@ int main(void)
     unsigned int VBO;// vertext buffer object holds the points
     glGenBuffers(1, &VBO);
 
-    //drawing an object
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
 
-    glBindBuffer(GL_ARRAY_BUFFER,VBO);//Binds the Buffer and the vertext object together so they can share info
+    //drawing an object
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);//Binds the Buffer and the vertext object together so they can share info
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);// copys verticies into the buffer
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);//tells open gl the format of the data given
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);//tells open gl the format of the data given
     glEnableVertexAttribArray(0);
 
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
     shader.use();//makes it the current rendering state
 
     while (!glfwWindowShouldClose(window))
@@ -57,6 +77,15 @@ int main(void)
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // render the triangle
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-	return 0;
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+    }
+    return 0;
 }
