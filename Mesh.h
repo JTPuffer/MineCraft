862b88d56d50a80e14c558823422f92129230534
JTPuffer <m.oliver4@lancaster@ac.uk>
2024-01-16 17:06:13 +0000

    mesh adding class

---
+++ Mesh.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <string>
+#include <vector>
+#include "Shader.h"
+
+using namespace std;
+
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
+struct Texture {
+	unsigned int id;
+	string type;
+};
+
+class Mesh {
+public:
+	// mesh data
+	vector<Vertex> vertices;
+	vector<unsigned int> indices;
+	vector<Texture> textures;
+	Mesh(vector<Vertex> vertices, vector<unsigned int> indices,
+		vector<Texture> textures);
+	void Draw(Shader& shader);
+private:
+	// render data
+	unsigned int VAO, VBO, EBO;
+	void setupMesh();
+};
